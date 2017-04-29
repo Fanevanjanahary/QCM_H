79eca961dc7550dfb92a99a1046427a7184c5f37
Proprietaire <Proprietaire@DESKTOP-H57HFA4>
2017-04-29 11:24:04 +0300

    add ejb GestionnaireReponse

---
+++ src/java/ejb/GestionnaireReponse.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejb;
+
+import entities.Reponse;
+import java.util.List;
+import javax.ejb.Stateful;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author ANDRIAMIADANTSOA
+ */
+@Stateful
+public class GestionnaireReponse {
+
+    @PersistenceContext(unitName = "QCM_HPU")
+    private EntityManager em;
+
+    public void creerReponse(Reponse reponse) {
+        em.persist(reponse);
+    }
+    
+    public Reponse update(Reponse r){
+        return em.merge(r);
+    }
+    
+    public List<Reponse> findAll()
+    {
+        Query query = em.createNamedQuery("Reponse.findAll");
+        return query.getResultList();
+    }
+    
+    public Reponse findById(Long id){
+        return em.find(Reponse.class, id);
+    }
+
+    public void delete(Reponse r){
+        em.remove(r);
+    }
+
+}
